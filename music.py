1f6fde7628b2cb89777553de0b5f2871a88561b9
QuinShark <s1002562@student.knoxschools.org>
2026-09-08 18:33:04 +0000

    retrying lyricsovh

--- music.py
+++ music.py
@@ -1,6 +1,6 @@
 from urllib import request, urlopen
 
-request = request('https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime')
+request = request('https://api.lyrics.ovh/v1/artist/title')
 
 response_body = urlopen(request).read()
 print (response_body)
