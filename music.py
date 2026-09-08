41a472c93db792b64b975fcacc37b68bbbda033d
QuinShark <s1002562@student.knoxschools.org>
2026-09-08 18:51:04 +0000

    failing poorly

--- music.py
+++ music.py
@@ -2,6 +2,15 @@ from urllib.request import Request, urlopen
 
 request = Request('https://api.lyrics.ovh/v1/artist/title')
 
-response_body = urlopen(request).read()
-print (response_body)
+# # parameters. still so very confused. 
+# # artist:Name of the artist
+# # title:Title of the song
+# {
+#     'lyrics':"Here the Lyrics of the song"
+# }
+# {
+#     "error": "No lyrics found"
+#         }
+# response_body = urlopen(request).read()
+# print (response_body)
 
